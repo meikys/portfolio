40ef525a6893461904b31d455b6feb338bc8116b
meikys <erik.dunca1@gmail.com>
2025-08-12 20:23:32 +0200

    edit: items-center

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
     >
       <div className="absolute top-1/2 left-1/2 lg:shadow-none w-[80vw] max-w-[700px] aspect-square bg-white rounded-full transform -translate-x-1/2 -translate-y-1/2 -z-0"></div>
       <div className="container flex flex-col lg:flex-row lg:flex-wrap justify-center items-center gap-[32px] ios-pb max-h-screen">
-        <div className="relative flex flex-col items-start text-center lg:text-left gap-[16px] z-20">
+        <div className="relative flex flex-col items-center text-center lg:text-left gap-[16px] z-20">
         <h1 className="text-[3rem] tracking-[-2px] leading-[110%] font-bold">
           {HeroText.title}
         </h1>
