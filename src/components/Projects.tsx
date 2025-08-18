131bf8b7637da3d8afcd2b3804e756688a214d23
meikys <erik.dunca1@gmail.com>
2025-08-19 00:52:05 +0200

    add: zakladna funkcionalita hoveru pri lg obrazovkach

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,17 +8,17 @@ import Button from "./ui/Button";
 export default function Projects() {
   return (
     <section
-      className="flex flex-col justify-center items-center gap-[32px] bg-[var(--bg-secondary)]"
+      className="flex flex-col justify-center items-center gap-[32px] lg:gap-0 bg-[var(--bg-secondary)]"
       id="#projects"
     >
       {projects.map(({ id, number, title, description, button, bgColor, imageSrc }) => (
-        <div key={id} className="flex flex-col-reverse gap-[32px] w-full min-h-[50vh] py-[64px]">
-          <div className="flex gap-[16px]">
+        <div key={id} className="flex flex-col-reverse lg:flex-row lg:justify-between gap-[32px] w-full min-h-[50vh] lg:min-h-[25vh] py-[64px] lg:py-0 group hover:bg-[#FFFFFF] transition ease-in-out duration-100 cursor-pointer">
+          <div className="flex gap-[16px] lg:w-[50%]">
             <div className="flex items-center gap-[8px]">
-              <hr className="w-[20px]" />
+              <hr className="w-[20px] lg:w-[20rem]" />
               <span>{number}</span>
             </div>
-            <div className="flex flex-col justify-end h-full gap-[8px] mr-10">
+            <div className="flex flex-col justify-end lg:justify-center h-full lg:h-auto gap-[8px] mr-10 lg:mr-0">
               <h3 className="text-[2rem] tracking-[-1.5%] leading-[130%] font-bold">
                 {title}
               </h3>
@@ -31,7 +31,7 @@ export default function Projects() {
             </div>
           </div>
           <div
-            className="w-[100%] h-auto relative"
+            className="w-[100%] lg:w-[50%] h-auto relative"
             style={{ perspective: "1000px" }}
           >
             <Image
@@ -39,9 +39,9 @@ export default function Projects() {
               width={800}
               height={800}
               alt={title}
-              className="relative top-0 max-w-[100vw] -translate-y-5 z-20 -rotate-y-20 shadow-xl"
+              className="relative top-0 max-w-[100vw] -translate-y-5 lg:group-hover:translate-x-[90%] lg:translate-x-[100vw] z-20 -rotate-y-20 shadow-xl lg:max-h-[320px] lg:max-w-[640px] ease-in-out duration-100"
             />
-            <div className={`absolute bottom-0 w-[300px] h-[230px] ${bgColor} z-10 opacity-50}`} />
+            <div className={`absolute bottom-0 w-[300px] lg:w-[600px] h-[230px] lg:h-[280px] lg:group-hover:translate-x-[90%] lg:translate-x-[100vw] ${bgColor} z-10 opacity-50} ease-in-out duration-100`} />
           </div>
         </div>
       ))}
