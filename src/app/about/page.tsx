d3e4d5b2ce25d4e294d27a985e353a35c13e5165
meikys <erik.dunca1@gmail.com>
2025-08-23 03:32:26 +0200

    add: nova podstranka /about s rozlozenim landing pagom

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,14 @@
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import DeveloperDesk from "@/components/pages/about/DeveloperDesk";
 
 export default function About() {
   return (
     <>
       <Navigation />
       <main>
+        <DeveloperDesk />
       </main>
       <footer>
         <Footer />
---
+++ src/components/pages/about/DeveloperDesk.tsx
@@ -0,0 +1,16 @@
+export default function DeveloperDesk() {
+  return (
+    <section className="flex flex-col justify-center items-center min-h-screen py-[64px] px-10 bg-[var(--bg-secondary)]">
+        <div className="container mx-auto grid place-items-center grid-cols-2 grid-rows-2 gap-[32px]">
+            <p className="text-[1rem] text-[#000000] tracking-[0.5%] leading-[150%] w-[500px] h-[500px] bg-red-200">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, eum tempore! Fugit hic quasi voluptates unde ea distinctio, cum nobis repellat labore, obcaecati sed explicabo necessitatibus natus qui delectus accusantium.
+            </p>
+            <div></div>
+            <div className="w-[500px] h-[500px] bg-red-200"></div>
+            <p className="text-[1rem] text-[#000000] tracking-[0.5%] leading-[150%] w-[500px] h-[500px] bg-red-200">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, eum tempore! Fugit hic quasi voluptates unde ea distinctio, cum nobis repellat labore, obcaecati sed explicabo necessitatibus natus qui delectus accusantium.
+            </p>
+        </div>
+    </section>
+  )
+}
