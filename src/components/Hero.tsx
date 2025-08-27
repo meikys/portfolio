646fd95eb9701e0c77c02fc34d8c8d4f52fdd1c7
meikys <erik.dunca1@gmail.com>
2025-08-27 22:04:35 +0200

    edit: pb na hero sekcii

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,11 +12,11 @@ import Button from "./ui/Button";
 export default function Hero() {
   return (
     <section
-      className="flex justify-center items-center bg-[var(--bg-secondary)] min-h-screen px-10"
+      className="flex justify-center items-start bg-[var(--bg-secondary)] min-h-screen px-10 pt-[55px] lg:pt-0"
       id="hero"
     >
       <div className="absolute top-1/2 left-1/2 lg:shadow-none w-[80vw] max-w-[700px] aspect-square bg-white rounded-full transform -translate-x-1/2 -translate-y-1/2 -z-0"></div>
-      <div className="container flex flex-col lg:flex-row lg:flex-wrap justify-center items-center gap-[32px]">
+      <div className="container flex flex-col lg:flex-row lg:flex-wrap justify-center items-center gap-[32px] pb-[55px] lg:pb-0">
         <div className="relative flex flex-col items-center lg:items-start text-center gap-[16px] z-20">
         <h1 className="text-[3rem] tracking-[-2px] leading-[110%] font-bold">
           {HeroText.title}
